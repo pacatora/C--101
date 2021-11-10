cd71c7d5f85d7b33b0ba19de9475dedd2a3d864a
pacatora <rabiagerz123@gmail.com>
2021-11-10 18:21:46 +0300

    operatorler

---
+++ Operatorler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a=3;
+           int b=5;
+           bool sonuc=a<b;
+
+
+           Console.WriteLine(sonuc);
+           sonuc=a>b;
+           Console.WriteLine(sonuc);
+           
+           sonuc= a==b;
+           Console.WriteLine(sonuc);
+           sonuc= a!=b;
+           Console.WriteLine(sonuc);
+
+
+
+           int sayi1=5;
+           int sayi2=3;
+           int sonuc1=sayi1+sayi2;
+           Console.WriteLine(sonuc1);
+           sonuc1=sayi1/sayi2;
+           Console.WriteLine(sonuc1);
+           sonuc1=sayi1%sayi2;
+           Console.WriteLine(sonuc1);
+           sonuc1+=1;
+           Console.WriteLine(sonuc1);
+        }
+    }
+}
