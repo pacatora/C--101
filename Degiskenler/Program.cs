5ecb5346272c849d7cc89984120599f65061e7a5
pacatora <rabiagerz123@gmail.com>
2021-11-10 16:31:34 +0300

    degiskenler eklendi

---
+++ Degiskenler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        string str20="20";
+        int int20=20;
+        string yeniDeger=str20+int20.ToString();
+        Console.WriteLine(yeniDeger);
+
+        int int21= int20+Convert.ToInt32(str20);
+        Console.WriteLine(int21);
+
+        int int22= int20+int.Parse(str20);
+        Console.WriteLine(int22);
+
+
+        string dateTime=DateTime.Now.ToString("dd/MM/yyyy");
+        Console.WriteLine(dateTime);
+
+        string hour=DateTime.Now.ToString("HH:mm");
+        Console.WriteLine(hour);
+        }
+
+       
+
+    }
+}
