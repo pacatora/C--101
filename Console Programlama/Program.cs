1253e8aa57e0035004d4ac466eb276d9fbd2d0c1
pacatora <rabiagerz123@gmail.com>
2021-11-10 13:32:12 +0300

    C# 101

---
+++ Console Programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Console_Programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı giriniz");
+            string ad= Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("Ad Soyad : "+ad+" "+soyad);
+        }
+    }
+}
